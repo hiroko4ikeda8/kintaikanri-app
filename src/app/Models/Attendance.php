589971ed030b8fafaf1bead6f0acbc5e71b933b1
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-05-19 11:06:48 +0900

    Added models and migrations for attendance, break times, and applications

---
+++ src/app/Models/Attendance.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    // Attendance.php
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function breakTimes()
+    {
+        return $this->hasMany(BreakTime::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasOne(Application::class);
+    }
+}
---
+++ src/database/migrations/2025_05_19_102508_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->time('clock_in');
+            $table->time('clock_out'); // 退勤前対応
+            $table->enum('status', ['勤務外', '勤務中', '休憩中', '退勤済']);
+            $table->date('attendance_date');
+            $table->integer('total_work_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
