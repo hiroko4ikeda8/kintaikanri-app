10fbd0ff3a749c4985241de65a618ca4bc74b17a
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-05-30 10:52:00 +0900

    ヘッダー　ナビ　リンク先作成　ログアウトボタン完了

--- src/resources/views/layouts/header/admin-header.blade.php
+++ src/resources/views/layouts/header/admin-header.blade.php
@@ -1,15 +1,16 @@
-<header class="header py-3 mb-4 border-bottom bg-dark">
+<header class="py-3 mb-4 border-bottom bg-dark">
     <div class="container d-flex align-items-center justify-content-between">
         <a href="{{ url('/') }}">
             <img src="{{ asset('images/logo.png') }}" alt="勤怠管理アプリ ロゴ" style="height: 50px;">
         </a>
-        <nav>
-            <a class="text-white text-decoration-none me-3" href="{{ route('admin.attendance.list') }}">勤怠一覧</a>
-            <a class="text-white text-decoration-none me-3" href="{{ route('admin.staff.index') }}">スタッフ一覧</a>
-            <a class="text-white text-decoration-none me-3" href="{{ route('admin.stamp_correction_request.list') }}">申請一覧</a>
-            <form method="POST" action="{{ route('logout') }}" class="d-inline">
+        <nav class="nav">
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('admin.attendance.list') }}">勤怠一覧</a>
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('admin.staff.index') }}">スタッフ一覧</a>
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('admin.stamp_correction_request.list') }}">申請一覧</a>
+            <form method="POST" action="{{ route('logout') }}" class="d-inline align-self-center">
                 @csrf
-                <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0 align-baseline">
+                <!-- <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0 align-baseline"> -->
+                <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0">
                     ログアウト
                 </button>
             </form>
--- src/resources/views/layouts/header/user-header.blade.php
+++ src/resources/views/layouts/header/user-header.blade.php
@@ -3,13 +3,14 @@
         <a href="{{ url('/') }}">
             <img src="{{ asset('images/logo.png') }}" alt="勤怠管理アプリ ロゴ" style="height: 50px;">
         </a>
-        <nav>
-            <a class="text-white text-decoration-none me-3" href="{{ route('user.attendance.create') }}">勤怠</a>
-            <a class="text-white text-decoration-none me-3" href="{{ route('user.attendance.list') }}">勤怠一覧</a>
-            <a class="text-white text-decoration-none me-3" href="{{ route('user.stamp_correction_request.list') }}">申請</a>
-            <form method="POST" action="{{ route('logout') }}" class="d-inline">
+        <nav class="nav">
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('user.attendance.create') }}">勤怠</a>
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('user.attendance.list') }}">勤怠一覧</a>
+            <a class="nav-link text-white text-decoration-none me-3" href="{{ route('user.stamp_correction_request.list') }}">申請</a>
+            <form method="POST" action="{{ route('logout') }}" class="d-inline align-self-center">
                 @csrf
-                <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0 align-baseline">
+                <!-- <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0 align-baseline"> -->
+                <button type="submit" class="btn btn-link text-white text-decoration-none p-0 m-0">
                     ログアウト
                 </button>
             </form>
