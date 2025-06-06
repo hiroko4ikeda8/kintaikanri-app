c550c52a869aaebfe8d3335179f3dede9b352f86
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-06-06 20:52:47 +0900

    user　勤怠詳細UI作成

--- src/resources/views/user/attendance/show.blade.php
+++ src/resources/views/user/attendance/show.blade.php
@@ -31,37 +31,59 @@
                   <tr style="height: 90px;">
                     <th style="padding-left: 50px; width: 30%; padding-top: 30px;">出勤・退勤</th>
                     <td style="width: 40%; padding-top: 20px;">
-                      <div class="d-flex align-items-center gap-2">
-                        <input
-                          type="text"
-                          id="clock_in"
-                          class="form-control"
-                          value="{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') }}"
-                          onchange="console.log('出勤時刻変更:', this.value)"
-                        >
-                        <span>～</span>
-                        <input
-                          type="text"
-                          id="clock_out"
-                          class="form-control"
-                          value="{{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') }}"
-                          onchange="console.log('退勤時刻変更:', this.value)"
-                        >
+                      <div class="d-flex align-items-center mb-2" style="width: 66.66%; gap: 0.5rem;">
+                        <div style="flex: 1;">
+                          <input
+                            type="text"
+                            id="clock_in"
+                            class="form-control"
+                            style="height: 35px;"
+                            value="{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') }}"
+                            onchange="console.log('出勤時刻変更:', this.value)"
+                          >
+                        </div>
+                        <div style="padding: 0 0.5rem; white-space: nowrap;">～</div>
+                        <div style="flex: 1;">
+                          <input
+                            type="text"
+                            id="clock_out"
+                            class="form-control"
+                            style="height: 35px;"
+                            value="{{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i') }}"
+                            onchange="console.log('退勤時刻変更:', this.value)"
+                          >
+                        </div>
                       </div>
                     </td>
                     <td></td>
                   </tr>
                   
-                  <tr style="height: 90px;">
+                  <tr style="height: 90px;"> 
                     <th style="padding-left: 50px; width: 30%; padding-top: 30px;">休憩</th>
-                    <td style="width: 40%; padding-top: 30px;">
+                    <td style="width: 40%; padding-top: 20px;">
                       @if ($attendance->breakTimes->isNotEmpty())
                         @foreach ($attendance->breakTimes as $index => $break)
-                          {{ \Carbon\Carbon::parse($break->break_start)->format('H:i') }} ～
-                          {{ \Carbon\Carbon::parse($break->break_end)->format('H:i') }}
-                          @if (!$loop->last)
-                            <br>
-                          @endif
+                          <div class="d-flex align-items-center mb-2" style="width: 66.66%; gap: 0.5rem;">
+                            <div style="flex: 1;">
+                              <input
+                                type="text"
+                                class="form-control"
+                                style="height: 35px;"
+                                value="{{ \Carbon\Carbon::parse($break->break_start)->format('H:i') }}"
+                                onchange="console.log('休憩{{ $index + 1 }}開始変更:', this.value)"
+                              >
+                            </div>
+                            <div style="padding: 0 0.5rem; white-space: nowrap;">～</div>
+                            <div style="flex: 1;">
+                              <input
+                                type="text"
+                                class="form-control"
+                                style="height: 35px;"
+                                value="{{ \Carbon\Carbon::parse($break->break_end)->format('H:i') }}"
+                                onchange="console.log('休憩{{ $index + 1 }}終了変更:', this.value)"
+                              >
+                            </div>
+                          </div>
                         @endforeach
                       @else
                         ―
@@ -69,20 +91,48 @@
                     </td>
                     <td></td>
                   </tr>
-
+                  
                   <tr style="height: 90px;">
                     <th style="padding-left: 50px; width: 30%; padding-top: 30px;">休憩2</th>
-                    <td style="width: 40%; padding-top: 30px;">
-                      {{ $attendance->break2_start ? \Carbon\Carbon::parse($attendance->break2_start)->format('H:i') : '―' }} ～ 
-                      {{ $attendance->break2_end ? \Carbon\Carbon::parse($attendance->break2_end)->format('H:i') : '―' }}
+                    <td style="width: 40%; padding-top: 20px;">
+                      <div class="d-flex align-items-center" style="width: 66.66%; gap: 0.5rem;">
+                        <div style="flex: 1;">
+                          <input
+                            type="text"
+                            id="break2_start"
+                            class="form-control"
+                            style="height: 35px;"
+                            value="{{ $attendance->break2_start ? \Carbon\Carbon::parse($attendance->break2_start)->format('H:i') : '' }}"
+                            onchange="console.log('休憩2開始変更:', this.value)"
+                          >
+                        </div>
+                        <div style="padding: 0 0.5rem; white-space: nowrap;">
+                          ～
+                        </div>
+                        <div style="flex: 1;">
+                          <input
+                            type="text"
+                            id="break2_end"
+                            class="form-control"
+                            style="height: 35px;"
+                            value="{{ $attendance->break2_end ? \Carbon\Carbon::parse($attendance->break2_end)->format('H:i') : '' }}"
+                            onchange="console.log('休憩2終了変更:', this.value)"
+                          >
+                        </div>
+                      </div>
                     </td>
                     <td></td>
                   </tr>
 
                   <tr style="height: 90px;">
-                    <th style="padding-left: 50px; width: 30%; padding-top: 30px;">備考</th>
-                    <td style="padding-top: 30px;">
-                      {{ $attendance->remarks ?? '―' }}
+                    <th style="padding-left: 50px; width: 30%; padding-top: 40px;">備考</th>
+                    <td style="padding-top: 20px; width: 40%;">
+                      <textarea
+                        class="form-control"
+                        style="max-width: 400px; height: 80px; resize: none;"
+                        placeholder="申請理由を入力"
+                        onchange="console.log('備考変更:', this.value)"
+                      >{{ $attendance->remarks ?? '' }}</textarea>
                     </td>
                     <td></td>
                   </tr>
